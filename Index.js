8310988abe3fe4f7ff7e05c817601b34727e6b63
JonasJ7 <jonasjuhlert7@gmail.com>
2022-03-25 15:32:53 +0100

    actually working..

--- Index.js
+++ Index.js
@@ -4,6 +4,7 @@ var h = 1000;
 var svg = d3.select("svg")
     .attr("width", w)
     .attr("height", h)
+    .style("background-color","black")
 
 var dataset = [];
 d3.json("dataset.json").then(function (data) {
@@ -13,25 +14,63 @@ d3.json("dataset.json").then(function (data) {
 
 var lineMaker = d3.line().curve(d3.curveCardinal);
 
-var pathData = lineMaker(coords)
+var xScale = d3.scaleLinear().domain([0, 24]).range([50, w-50]);
+var yScale = d3.scaleLinear().domain([1, 12]).range([h/2, 50]);
 
+var arrData = []
+var nested = []
+var g;
 function drawLine() {
-    var xScale = d3.scaleLinear().domain([0, dataset.length]).range([0, w]);
-var yScale = d3.scaleLinear().domain([0, 100]).range([h, 0]);
+ 
+    for (var i = 0; i < dataset.length; i++) {
+        for (var j = 0; j<dataset[i].time.length; j++){
+            arrData.push({
+                "x": dataset[i].time[j],
+                "y": dataset[i].altitude[j],
+                "date": dataset[i].date
+            })
+        }
+    }
 
-lineMaker
-    .x(function (d, i) {
-        return xScale(i);
+    nested = d3.group(arrData, function(d){
+        return d.date;
     })
-    .y(function (d) {
-        return yScale(d.altitude);
-    });
 
-    var lineData = lineMaker(dataset);
+    lineMaker
+        .x(function (d) {
+            return xScale(d.x);
+        })
+        .y(function (d) {
+            return yScale(d.y);
+        });
+    var lineData;
+    g = svg.selectAll('g')
+        .data(nested)
+        .join('g')
+        .attr('transform',function(d,i){
+            var x = i;
+            return 'translate('+x+','+h/4+')'
+        })
 
-   svg
-    .append('path')
-   .attr('d', lineData)
-       .attr('stroke', 'white')
-       .attr('fill', "none")
+    var pathShape = g.append('path')
+        .attr('d',function(d,i){
+            var lineData = (lineMaker(d[1]));
+            return lineData
+        })
+        .attr('stroke', function(d) { 
+            return 'white'
+        })
+        .attr('fill', "none")
+
+    // var circs = g.selectAll('circle')
+    //     .data(function(d){
+    //         return d[1];
+    //     })
+    //     .join('circle')
+    //     .attr('cx',0)
+    //     .attr('cy',0)
+    //     .attr('r',function(d){
+    //         return d.y;
+    //     })
+    //     .attr('stroke','white')
 }
